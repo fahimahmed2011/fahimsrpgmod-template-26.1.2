538922e4c579d3c0b017df0123d85030d42a6c34
mfahimahmed2011-hash <mfahimahmed2011@gmail.com>
2026-06-05 22:19:26 +0100

    LootTables

--- src/main/java/net/fahim/fahimsrpgmod/FahimsRPGModDataGen.java
+++ src/main/java/net/fahim/fahimsrpgmod/FahimsRPGModDataGen.java
@@ -1,11 +1,18 @@
 package net.fahim.fahimsrpgmod;
 
+import net.fahim.fahimsrpgmod.datagen.ModBlockLootTableProvider;
+import net.fahim.fahimsrpgmod.datagen.ModBlockTagsProvider;
 import net.fahim.fahimsrpgmod.datagen.ModModelprovider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.Collections;
+import java.util.List;
 
 @EventBusSubscriber(modid = FahimsRPGMod.MOD_ID)
 public class FahimsRPGModDataGen {
@@ -13,9 +20,13 @@ public class FahimsRPGModDataGen {
     public static void gatherClientData(GatherDataEvent.Client event) {
     DataGenerator generator = event.getGenerator();
     PackOutput packOutput = generator.getPackOutput();
+    var lookupProvider = event.getLookupProvider();
 
 
     generator.addProvider(true,new ModModelprovider(packOutput));
+    generator.addProvider(true,new ModBlockTagsProvider(packOutput,lookupProvider));
+    generator.addProvider(true, new LootTableProvider(packOutput, Collections.emptySet(),
+            List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
 
     }
 }
--- src/main/java/net/fahim/fahimsrpgmod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/fahim/fahimsrpgmod/datagen/ModBlockLootTableProvider.java
@@ -1,4 +1,69 @@
 package net.fahim.fahimsrpgmod.datagen;
 
-public class ModBlockLootTableProvider {
+import net.fahim.fahimsrpgmod.block.ModBlocks;
+import net.fahim.fahimsrpgmod.item.ModItems;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    public ModBlockLootTableProvider( HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.AZURITE_BLOCK.get());
+        dropSelf(ModBlocks.RAW_AZURITE_BLOCK.get());
+        dropSelf(ModBlocks.PINK_GARNET_BLOCK.get());
+        dropSelf(ModBlocks.RAW_PINK_GARNET_BLOCK.get());
+
+        add(ModBlocks.AZURITE_ORE.get(),
+                createOreDrop(ModBlocks.AZURITE_ORE.get(),ModItems.RAW_AZURITE.get()));
+
+        add(ModBlocks.AZURITE_ORE.get(),
+                createOreDrop(ModBlocks.AZURITE_DEEPSLATE_ORE.get(),ModItems.RAW_AZURITE.get()));
+
+        add(ModBlocks.AZURITE_NETHER_ORE.get(),
+        createMultipleOreDrops(ModBlocks.AZURITE_NETHER_ORE.get(),ModItems.RAW_AZURITE.get(),4,7));
+
+        add(ModBlocks.AZURITE_END_ORE.get(),
+        createMultipleOreDrops(ModBlocks.AZURITE_END_ORE.get(),ModItems.RAW_AZURITE.get(),4,7));
+
+        add(ModBlocks.PINK_GARNET_ORE.get(),
+                createMultipleOreDrops(ModBlocks.PINK_GARNET_ORE.get(),ModItems.RAW_PINK_GARNET.get(),4,5));
+
+        add(ModBlocks.AZURITE_DEEPSLATE_ORE.get(),
+                createMultipleOreDrops(ModBlocks.AZURITE_DEEPSLATE_ORE.get(),ModItems.RAW_AZURITE.get(),4,5));
+
+        add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE.get(),
+                createMultipleOreDrops(ModBlocks.PINK_GARNET_DEEPSLATE_ORE.get(),ModItems.RAW_PINK_GARNET.get(),4,5));
+    }
+    protected LootTable.Builder createMultipleOreDrops(Block block, Item item, float minDrops, float maxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> enchantments = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(block, this.applyExplosionDecay(block,
+                LootItem.lootTableItem(item)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                        .apply(ApplyBonusCount.addOreBonusCount(enchantments.getOrThrow(Enchantments.FORTUNE)))));
+    }
+
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
 }
